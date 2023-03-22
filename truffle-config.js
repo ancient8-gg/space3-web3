ca4b84561994647b232acdb982017b00072ea440
dungphan180 <dungphan@ancient8.gg>
2023-03-22 14:55:11 +0700

    update compile files

--- truffle-config.js
+++ truffle-config.js
@@ -90,7 +90,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.13", // Fetch exact version from solc-bin
+      version: "0.8.19", // Fetch exact version from solc-bin
     },
   },
 };
