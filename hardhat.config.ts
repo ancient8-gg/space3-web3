fae23e8bf4eb5a282b3529e80cc7474c2162fecf
Richard <thanhle@ancient8.gg>
2024-01-08 18:03:38 +0700

    feat: add hardhat verify fix: remove test branch in release config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,13 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-deploy';
+import * as Dotenv from 'dotenv';
+Dotenv.config();
+
+const ADMIN_PK = process.env.ADMIN_PK || '';
+const ACCOUNTS = [ADMIN_PK];
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -15,11 +23,47 @@ const config: HardhatUserConfig = {
     ancient8Testnet: {
       url: 'https://rpc-testnet.ancient8.gg',
       chainId: 2863311531,
+      accounts: ACCOUNTS,
+      saveDeployments: true,
+      allowUnlimitedContractSize: false,
     },
     ancient8TestnetV2: {
       url: 'https://rpcv2-testnet.ancient8.gg',
       chainId: 28122024,
+      accounts: ACCOUNTS,
+      saveDeployments: true,
+      allowUnlimitedContractSize: false,
     },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  mocha: {},
+  etherscan: {
+    apiKey: {
+      ancient8Testnet: process.env.ANCIENT8_TESTNET_API_KEY || '',
+      ancient8TestnetV2: process.env.ANCIENT8_TESTNET_V2_API_KEY || '',
+    },
+    customChains: [
+      {
+        network: 'ancient8Testnet',
+        chainId: 2863311531,
+        urls: {
+          apiURL: 'https://testnet.a8scan.io/api',
+          browserURL: 'https://testnet.a8scan.io',
+        },
+      },
+      {
+        network: 'ancient8TestnetV2',
+        chainId: 28122024,
+        urls: {
+          apiURL: 'https://scanv2-testnet.ancient8.gg/api',
+          browserURL: 'https://scanv2-testnet.ancient8.gg',
+        },
+      },
+    ],
   },
   typechain: {
     outDir: './sdk/typescript/typechain',
