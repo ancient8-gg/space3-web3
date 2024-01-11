2b96e8daacadedbac4fc6a7c731a112209c60348
Richard <thanhle@ancient8.gg>
2024-01-11 11:18:44 +0700

    feat: space3 genesis sdk

--- sdk/typescript/index.ts
+++ sdk/typescript/index.ts
@@ -1,5 +1,1 @@
-export * from './typechain/contracts/A8-NFT-1155.sol';
-export * from './typechain/contracts/A8-NFT.sol';
-
-export * from './typechain/factories/contracts/A8-NFT-1155.sol';
-export * from './typechain/factories/contracts/A8-NFT.sol';
+export * from './open-edition';
